f14b96ee261ccd874a30b95559b07fdedcd5e4b2
Pbsmith82 <pbsmith82@gmail.com>
2020-09-25 15:35:13 -0400

    Commented Suggested Solutions

--- lib/song.rb
+++ lib/song.rb
@@ -9,32 +9,32 @@ class Song
   end
 
   def self.create
-    song = self.new
-    @@all << song
+    song = self.new #Song.new
+    @@all << song #song.save
     song
   end
 
   def self.new_by_name(name)
     song = self.new
     song.name = name
-    @@all << song
+    @@all << song # Not Needed by test
     song  
   end
 
   def self.create_by_name(name)
-    song = self.new
+    song = self.new # song = self.create
     song.name = name
-    @@all << song
+    @@all << song # Not needed if used the .create
     song  
   end
 
   def self.find_by_name(name)
-    self.all.find {|song|song.name == name}
+    self.all.find {|song|song.name == name} # self.all.detect{|s| s.name == song_name}
   end
 
   def self.find_or_create_by_name(name)
     self.find_by_name(name) ||
-    self.create_by_name(name) 
+    self.create_by_name(name) # Formating this should be on the same line
   end
 
   def self.alphabetical
@@ -54,7 +54,28 @@ class Song
     song.artist_name = file[0].strip
     song
   end
+# This are suggest for the two Sections Above
+# def self.new_from_filename(filename)
+#   parts = filename.split(" - ")
+#   artist_name = parts[0]
+#   song_name = parts[1].gsub(".mp3", "")
 
+#   song = self.new
+#   song.name = song_name
+#   song.artist_name = artist_name
+#   song
+# end
+
+# def self.create_from_filename(filename)
+#   parts = filename.split(" - ")
+#   artist_name = parts[0]
+#   song_name = parts[1].gsub(".mp3", "")
+
+#   song = self.create
+#   song.name = song_name
+#   song.artist_name = artist_name
+#   song
+# end
   def self.destroy_all
     @@all.clear
   end
