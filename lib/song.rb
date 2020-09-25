77b41f3b2fc313ba7cc473907a8e6090fab64d13
Pbsmith82 <pbsmith82@gmail.com>
2020-09-25 15:21:09 -0400

    advanced methods

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require"pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -6,6 +8,57 @@ class Song
     @@all
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song  
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song  
+  end
+
+  def self.find_by_name(name)
+    self.all.find {|song|song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ||
+    self.create_by_name(name) 
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+  def self.new_from_filename (filename)
+    file = filename.split(/[-.]+/)
+    song = self.new_by_name(file[1].strip)
+    song.artist_name = file[0].strip
+    song
+  end
+
+  def self.create_from_filename (filename)
+    file = filename.split(/[-.]+/)
+    song = self.create_by_name(file[1].strip)
+    song.artist_name = file[0].strip
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+  #binding.pry
   def save
     self.class.all << self
   end
